40b5bf90acb5bd2c374a369721f563e9625dd004
Max Zakharzhevskiy <maxzz@msn.com>
2025-12-26 19:39:56 -0800

    WIP. add deploy to github

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    nodePolyfills({
-      include: ['path', 'stream', 'util', 'process', 'buffer', 'assert', 'os', 'constants'],
-      globals: {
-        Buffer: true,
-        global: true,
-        process: true,
-      },
-    }),
-  ],
-})
+    base: '',
+    build: {
+        chunkSizeWarningLimit: 500,
+    },
+    plugins: [
+        react(),
+        tailwindcss(),
+        nodePolyfills({
+            include: ['path', 'stream', 'util', 'process', 'buffer', 'assert', 'os', 'constants'],
+            globals: {
+                Buffer: true,
+                global: true,
+                process: true,
+            },
+        }),
+    ],
+});
