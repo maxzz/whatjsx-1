31c49c2ae0523a828c87b8242608e88d73c57dde
Max Zakharzhevskiy <maxzz@msn.com>
2025-12-29 16:22:06 -0800

    WIP. tweak default shadcn dialog. 3

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
@@ -36,4 +37,10 @@ export default defineConfig({
             },
         }),
     ],
+    resolve: {
+        alias: {
+            '@': path.join(__dirname, 'src'),
+            '@ui': path.join(__dirname, 'src/components/ui/'),
+        }
+    }
 });
